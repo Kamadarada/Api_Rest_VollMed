4d167c4071b8f3ccc5b84af4cb296d6a0e1b823a
Kamadarada <felipekamada2@gmail.com>
2023-08-02 22:55:28 -0300

    Criação da listagem de médicos: Criação do método PostMapping no controller de médicos; Método de list<DadosListagemMedicos>; Criação de records para devolver mapear DTO; Conversão no controller de JPA para DTO

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,12 +5,15 @@ import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.Endereco.Endereco;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.ErrorResponseException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -19,8 +22,8 @@ public class MedicoController {
     @Autowired
     private MedicoRepository repository;
 
-    @PostMapping
-    @Transactional
+    @PostMapping //verbo do protocolo http
+    @Transactional //contextos de transação
     //Método cadastrar recebe dto de DadosCadastroMedico
     public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
 
@@ -29,4 +32,10 @@ public class MedicoController {
         return ResponseEntity.ok("Sucesso! Médico cadastrado na base de dados VollMed");
 
     }
+
+    @GetMapping
+    public List<DadosListagemMedico> ListarMedico(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList(); //converter Lista de medico para lista DadosListagemMedico (dto);
+    }
 }
+
